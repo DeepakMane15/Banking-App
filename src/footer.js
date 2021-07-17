08ce696feedabe3b8c56be1e937ba2fc0a113167
Deepak Mane <manedeep2001@gmail.com>
2021-07-17 22:38:21 +0530

    commit

--- src/footer.js
+++ src/footer.js
@@ -6,8 +6,8 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { Icon, InlineIcon } from '@iconify/react';
-import mailIcon from '@iconify-icons/ci/mail';
-import phoneIcon from '@iconify-icons/ci/phone';
+// import mailIcon from '@iconify-icons/ci/mail';
+// import phoneIcon from '@iconify-icons/ci/phone';
 
 
 
@@ -44,10 +44,10 @@ class Footer extends Component {
                 <h4 style={{color: "white"}} >Have Any Questions?</h4>
                <ul className="list-unstyled" style={{color: "gray"}}>
                    <li>
-                       <Icon icon={mailIcon}/> support.sai.bank@gmail.com
+                       support.sai.bank@gmail.com
                    </li>
                    <li>
-                   <Icon icon={phoneIcon}/> 8888888888
+                    8888888888
                    </li>
                </ul>
                
